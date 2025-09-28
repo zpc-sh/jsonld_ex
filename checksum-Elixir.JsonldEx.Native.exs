fdd38bc04ebc4159f593ce1080d5926bfc5a0658
Loc Nguyen <l@nocsi.com>
2025-09-28 05:04:35 -0700

    Add Turbo benchmark results for NIF library

--- checksum-Elixir.JsonldEx.Native.exs
+++ checksum-Elixir.JsonldEx.Native.exs
@@ -1,3 +1,11 @@
 %{
-  "libjsonld_nif-v0.4.4-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:40cb69b85f7cd699c4124c75bcc5c0d6de6ee9e3cab7ec7958d91cec6cfb1e59",
+  "libjsonld_nif-v0.4.4-nif-2.14-aarch64-apple-darwin.so.tar.gz" => "sha256:473d35d3658bc1b9ded63e5f0484104126cc75d7b006ec3fbaa3e5553a91950b",
+  "libjsonld_nif-v0.4.4-nif-2.14-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:8e7bcc76476608e553de5f8505e3749f98f3fba79769d2a58072dfc5515d500a",
+  "libjsonld_nif-v0.4.4-nif-2.14-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:097189a13dbf98238a2e027f0575fc293b0a119743105e292f6f31d6713c72c7",
+  "libjsonld_nif-v0.4.4-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:34aaf3f67a882e2a7614e502dfc4141a1394949506add1fdec4777378368a514",
+  "libjsonld_nif-v0.4.4-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:a0e54996b146010146cb7f3a23a3cbe61bade89684517afceb4a2c80698ae21f",
+  "libjsonld_nif-v0.4.4-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:0403f22ea4d6293645e4237e91c37843455df773a14c9e307a8a0f257aa74e33",
+  "libjsonld_nif-v0.4.4-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:2974a74eec69937d8c0b0480ae62b0aa1457962a90d625aa3cd9fcd33426fc16",
+  "libjsonld_nif-v0.4.4-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:681245c7fbcbaf4fcc6f463e3ff14a52fa0e5a8c8f6f0c4c550ef4fed600d0a6",
+  "libjsonld_nif-v0.4.4-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:10db90d98136545ace1d3655f4b589dcfa11de6c4262f2c3fa44a9d706cbc863",
 }
