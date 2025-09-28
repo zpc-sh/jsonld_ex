c01e29e0f3cbf0cca7e3b98ebdd5ffc175d18c75
Loc Nguyen <l@nocsi.com>
2025-09-28 04:19:46 -0700

    to Makefile Update checksum for jsonld native library binary

--- checksum-Elixir.JsonldEx.Native.exs
+++ checksum-Elixir.JsonldEx.Native.exs
@@ -1,11 +1,3 @@
 %{
-  "libjsonld_nif-v0.4.5-nif-2.14-aarch64-apple-darwin.so.tar.gz" => "sha256:20dbef622050f6f5e7f0a57289955e028cf00a50998833ae88acc3d7e833c24b",
-  "libjsonld_nif-v0.4.5-nif-2.14-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:cc62abe64809f2eb174156325ee86194f6cd0b44e8f381657ab92599f24a3fa4",
-  "libjsonld_nif-v0.4.5-nif-2.14-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:5bf1d85f83fdda98f86d703a18920fe7ee3e46ee0ca395522a850536a2f28c21",
-  "libjsonld_nif-v0.4.5-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:cf318e066825c1600d130c55b2a1980c6fea980f62970fe8dd2375bce7b6baa8",
-  "libjsonld_nif-v0.4.5-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:341e6de85c64bd018a37df43a0c65e4e5d31cb6ff05b3f64c3e31a1827eab078",
-  "libjsonld_nif-v0.4.5-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:39807b0a294ac47d28609c76cbafcf5fea84f9acef6e99bfda2906f5bcc5a401",
-  "libjsonld_nif-v0.4.5-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:4155e067584d3fa76e429700389d34aa39fce1c13115d1f5c87760b044de163b",
-  "libjsonld_nif-v0.4.5-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:097205455c75c0ecb16244cad4246ff4daee44550a2c5d34f78fc449d833b915",
-  "libjsonld_nif-v0.4.5-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:5bf1d85f83fdda98f86d703a18920fe7ee3e46ee0ca395522a850536a2f28c21",
+  "libjsonld_nif-v0.4.2-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:d3a50718646f4539c88bb7873d3469a14b2b609fe7dff1ccf209acfa7d36ed38",
 }
